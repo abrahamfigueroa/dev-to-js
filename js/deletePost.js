e7e5d2d36f150eb35b13af5bcbc01a7f03eb9f70
Bryan Rodriguez <b.ra@live.com.mx>
2022-11-14 17:08:30 -0600

    making it a method

--- js/deletePost.js
+++ js/deletePost.js
@@ -1,7 +1,11 @@
 import db from "./environment.js";
 
+const deleteTask = (id) => {
+
 fetch(`${db}/${id}.json`, { //agregar variable ID del objeto a eliminar
   method: "DELETE",
 })
   .then((res) => res.json())
   .then((data) => console.log("Elemento eliminado", data));
+
+};
