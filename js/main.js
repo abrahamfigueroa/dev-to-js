80453600f24b5af100eb18765bffcce19d6cb557
Bryan Rodriguez <b.ra@live.com.mx>
2022-11-14 19:38:56 -0600

    getAllPosts method

--- js/main.js
+++ js/main.js
@@ -3,11 +3,32 @@ import db from "./environment.js";
 // Método eliminar
 
 const deletePost = (id) => {
+  fetch(`${db}/${id}.json`, {
+    //agregar variable ID del objeto a eliminar
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((data) => console.log("Elemento eliminado", data));
+};
 
-    fetch(`${db}/${id}.json`, { //agregar variable ID del objeto a eliminar
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => console.log("Elemento eliminado", data));
-    
-    };
+// Método Get all posts
+
+const getAllPosts = () => {
+  fetch(db + ".json", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      const keys = Object.keys(result);
+      const idTareas = keys.reduce((prev, act) => {
+        const tareaAct = result[act];
+        const tareaCompleta = {
+          id: act,
+          ...tareaAct,
+        };
+        prev.push(tareaCompleta);
+        return prev;
+      }, []);
+      console.log(idTareas);
+    });
+};
