fa3e3644c99ce5b0a49bb1eee8192fa481813852
Bryan Rodriguez <b.ra@live.com.mx>
2022-11-14 20:05:46 -0600

    getallpost consumes the cardCreation function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import db from "./environment.js";
+const cardsContainer = document.querySelector("#cardsContainer");
 
 // Método eliminar
 
@@ -30,5 +31,61 @@ const getAllPosts = () => {
         return prev;
       }, []);
       console.log(idTareas);
+      for (const post of idTareas) {
+        const card = cardCreation(post.title, post.description);
+        cardsContainer.appendChild(card);
+      };
     });
 };
+
+getAllPosts();
+
+const cardCreation = (nombre, descripcion, imagen, fechaDeCreacion, etiquetas) => {
+    const card = document.createElement("div");
+    card.classList.add("card", "mb-2");
+  
+    const img = document.createElement("img"); //Imagen de la card
+    img.classList.add("card-img-top");
+    img.src = imagen;
+  
+    const cardBody = document.createElement("div"); // Este es el div del cuerpo de la card
+    cardBody.classList.add("card-body");
+  
+    const creationDate = document.createElement("p"); // Fecha de creación del post
+    creationDate.classList.add("card-text");
+    creationDate.innerHTML = fechaDeCreacion;
+  
+    const name = document.createElement("h5"); // Título del post
+    name.classList.add("h5", "card-title");
+    name.innerHTML = "<strong>"+nombre+"</strong>";
+  
+    // const tagsArray = document.createElement("div");
+    // tagsArray.classList.add("d-flex justify-content-start");
+    
+    // const tags = document.createElement("p");
+    // tags.innerHTML = etiquetas;
+  
+    const description = document.createElement("p"); // Descripción del post
+    description.classList.add("card-text");
+    description.innerHTML = descripcion;
+  
+    const editButton = document.createElement("a"); // Botón editar
+    editButton.classList.add("btn", "btn-primary", "me-2");
+    editButton.innerText = "Editar";
+  
+    const deleteButton = document.createElement("a"); // Boton eliminar
+    deleteButton.classList.add("btn", "btn-danger");
+    deleteButton.innerText = "Borrar";
+  
+    if(imagen!=null)cardBody.appendChild(img); // Si el campo de imagen esta vacío, se omite añadir este elemento al DOM
+    if(fechaDeCreacion!=null)cardBody.appendChild(creationDate); // Si el campo de fecha de creación esta vacío, se omite añadir este elemento al DOM
+    cardBody.appendChild(name);
+    cardBody.appendChild(description);
+    // cardBody.appendChild(tagsArray);
+    // if(etiquetastags!=null) tagsArray.appendChild(tags);
+    cardBody.appendChild(editButton);
+    cardBody.appendChild(deleteButton);
+    card.appendChild(cardBody);
+  
+    return card;
+  };
