f26de16c0c2d983e90d4ca6a234e24dacb503141
Bryan Rodriguez <b.ra@live.com.mx>
2022-11-14 19:00:04 -0600

    deletePost method

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 import db from "./environment.js";
 
+// Método eliminar
+
+const deletePost = (id) => {
+
+    fetch(`${db}/${id}.json`, { //agregar variable ID del objeto a eliminar
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("Elemento eliminado", data));
+    
+    };
